4abbd3dae0def2d99a2e9a33f7c16ffab2c71b1b
David Parsley <davidparsley.kakhayanga@gmail.com>
2025-01-31 20:49:06 +0300

    Creates login in UserContext

---
+++ frontend/src/context/Usercontext.jsx
@@ -0,0 +1,148 @@
+import { createContext, useEffect, useState } from "react";
+import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
+
+export const UserContext = createContext();
+
+export const UserProvider = ({ children }) => {
+    
+  const navigate = useNavigate();
+  const [authToken, setAuthToken] = useState(() =>
+    sessionStorage.getItem("token")
+  );
+
+  const [current_user, setCurrentUser] = useState(null);
+  const [current_admin, setCurrentAdmin] = useState(null);
+  const [users, setUsers] = useState([])
+  // const [onChange, setOnChange] = useState(true);
+
+// login
+  const login = (email, password) => {
+    toast.loading("Logging you in ... ");
+    fetch("http://127.0.0.1:5000/login", {
+      method: "POST",
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify({ email, password }),
+    })
+      .then((resp) => resp.json())
+      .then((response) => {
+        if (response.access_token) {
+          toast.dismiss();
+          sessionStorage.setItem("token", response.access_token);
+          setAuthToken(response.access_token);
+
+          fetch("http://127.0.0.1:5000/current_user", {
+            method: "GET",
+            headers: {
+              "Content-type": "application/json",
+              Authorization: `Bearer ${response.access_token}`,
+            },
+          })
+            .then((response) => response.json())
+            .then((response) => {
+              console.log(response); 
+
+              if (response.is_admin) {
+                setCurrentAdmin(response); 
+              } else {
+                setCurrentUser(response);               }
+            });
+
+          toast.success("Successfully Logged in");
+          navigate("/");
+        } else if (response.error) {
+          toast.dismiss();
+          toast.error(response.error);
+        } else {
+          toast.dismiss();
+          toast.error("Failed to login");
+        }
+      });
+  };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
