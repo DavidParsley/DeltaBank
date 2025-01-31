d16465e88ff84154013f1ca93956235a3e8d809a
David Parsley <davidparsley.kakhayanga@gmail.com>
2025-01-31 21:16:24 +0300

    Adds data in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./index.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom"; 
+import { LoanProvider } from "./context/LoanContext";
+import Layout from "./components/Layout";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import NoPage from "./pages/NoPage";
+import Register from "./pages/Register";
+import Profile from "./pages/Profile";
+import SingleLoan from "./pages/SingleLoan";
+import AddLoan from "./pages/AddLoan";
+import About from "./pages/About";
+import Accounts from "./pages/Accounts";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <BrowserRouter>
+      <UserProvider>
+        <LoanProvider>
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route index element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/accounts" element={<Accounts />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/singleloan/:id" element={<SingleLoan />} />
+              <Route path="/addloan" element={<AddLoan />} />
+              <Route path="*" element={<NoPage />} />
+            </Route>
+          </Routes>
+        </LoanProvider>
+      </UserProvider>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
